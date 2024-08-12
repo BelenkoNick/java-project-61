f6e68ae251213907c05d43e60567e518ee3fb84f
BelenkoNick <belenko.nr@gmail.com>
2024-08-12 12:34:17 +0300

    Правки по автопроверке

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,17 +1,17 @@
 package hexlet.code;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Engine {
 
-    private static final Scanner scn = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final int ITERATION_COUNT = 3;
 
     public static void gameLoop(String name, String[] questions, String[] answers) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < ITERATION_COUNT; i++) {
             System.out.println("Question: " + questions[i]);
             System.out.print("Your answer: ");
-            String userAnswer = scn.nextLine();
+            String userAnswer = SCANNER.nextLine();
             if (userAnswer.equals(answers[i])) {
                 System.out.println("Correct!");
             } else {
--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -7,17 +7,19 @@ import java.util.Random;
 
 public class Even {
 
-    private static final Random rnd = new Random();
+    private static final Random RANDOM = new Random();
+    private static final int ITERATION_COUNT = 3;
+    private static final int BOUND = 10;
 
     public static void start() {
         String name = Cli.greetings();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
-        String[] questions = new String[3];
-        String[] answers = new String[3];
+        String[] questions = new String[ITERATION_COUNT];
+        String[] answers = new String[ITERATION_COUNT];
 
-        for (int i = 0; i < 3; i++) {
-            int randomInt = rnd.nextInt(10);
+        for (int i = 0; i < ITERATION_COUNT; i++) {
+            int randomInt = RANDOM.nextInt(BOUND);
             questions[i] = String.valueOf(randomInt);
             answers[i] = randomInt % 2 == 0 ? "yes" : "no";
         }
